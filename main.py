55a28cea90ced406556bc17f913c4342fc3a2f8f
Samuel A <126733372+ClockWorkCosmos@users.noreply.github.com>
2024-01-19 14:30:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def find_similarity(REFERENCE, COMPARISON):
 		if m.isclose(comp_value, ref_value):
 			similarity_score += 1
 		else:
-			similarity_score += -1
+			similarity_score += 0
 
 	similarity_percentile = (similarity_score / len(REFERENCE)) * 100
 
