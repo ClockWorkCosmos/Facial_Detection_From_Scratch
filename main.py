78b34fb4a27dc84a55b201dcbdcfd4c7f438c34d
Samuel A <126733372+ClockWorkCosmos@users.noreply.github.com>
2024-01-19 22:02:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 import math as m
 import os
 import pygame
@@ -24,6 +25,24 @@ solutions_set = [0.0, 0.0, 0.0, 0.0]
 
 result = str("")
 
+def zoom_effect(image_path, zoom_factor):
+	img = cv2.imread(image_path)
+
+	height, width = img.shape[:2]
+	center = (width // 2, height // 2)
+
+	zoom_matrix = cv2.getRotationMatrix2D(center, 1, zoom_factor)
+
+	zoomed_img = cv2.warpAffine(img, zoom_matrix, (width, height))
+	cv2.imwrite(image_path, zoomed_img)
+
+def reverse_zoom_effect(image_path, original_data):
+	v2.imwrite(image_path, original_data)
+
+def copy_image_data(image_path):
+	img = cv2.imread(image_path)	
+	return img
+
 def fetch_image_data(IMAGE_PATH):
 	image = pygame.image.load(IMAGE_PATH)
 	width, height = image.get_size()
@@ -34,34 +53,35 @@ def fetch_image_data(IMAGE_PATH):
 			pixel = image.get_at((x, y))
 			rgb_values = (pixel.r, pixel.g, pixel.b)
 			rgb_data.append(rgb_values)
-
 	return rgb_data
 
 def fetch_greyscale_data(IMAGE_PATH):
 	image = pygame.image.load(IMAGE_PATH)
+
 	width, height = image.get_size()
 
 	greyscale_data = []
+
 	for y in range(height):
 		for x in range(width):
 			pixel = image.get_at((x, y))
 			greyscale_value = int(0.3 * pixel.r + 0.59 * pixel.g + 0.11 * pixel.b)
 			greyscale_data.append(greyscale_value)
-
 	return greyscale_data
 
 def fetch_blackwhite_data(IMAGE_PATH, threshold=128):
 	image = pygame.image.load(IMAGE_PATH)
+
 	width, height = image.get_size()
 
 	blackwhite_data = []
+
 	for y in range(height):
 		for x in range(width):
 			pixel = image.get_at((x, y))
 			average_value = (pixel.r + pixel.g + pixel.b) // 3
 			blackwhite_value = 0 if average_value < threshold else 255
 			blackwhite_data.append(blackwhite_value)
-
 	return blackwhite_data
 
 def find_similarity(REFERENCE, COMPARISON):
@@ -76,13 +96,12 @@ def find_similarity(REFERENCE, COMPARISON):
 			similarity_score += 1
 		else:
 			similarity_score += 0
-
 	similarity_percentile = (similarity_score / len(REFERENCE)) * 100
-
 	return abs(similarity_percentile)
 
 def prRed(skk): 
 	print("\033[91m {}\033[00m" .format(skk)) 
+
 def prGreen(skk): 
 	print("\033[92m {}\033[00m" .format(skk))
 
@@ -90,10 +109,15 @@ comparison_path = input(">> Test Photo: ")
 
 while True:
 	reference_path = "database/face.png"
-
+	original_reference_data = [0]
 	reference_image = fetch_image_data(reference_path)
 
 	reference_directory = "database"
+
+	original_reference_data = copy_image_data(reference_path)
+
+	zoom_factor = 0.15
+	zoom_effect(reference_path, zoom_factor)
 
 	if os.path.exists("similarity_threshold.txt"):
 		with open("similarity_threshold.txt", "r") as file:
@@ -107,6 +131,10 @@ while True:
 				additional_image_path = os.path.join(reference_directory, filename)
 				additional_images.append(additional_image_path)
 
+				original_image_data = copy_image_data(additional_image_path)
+
+				zoom_effect(additional_image_path, zoom_factor)
+
 				additional_image_data = fetch_image_data(additional_image_path)
 				solutions_set.append(find_similarity(reference_image, additional_image_data))
 
@@ -116,23 +144,35 @@ while True:
 				additional_image_data = fetch_greyscale_data(additional_image_path)
 				solutions_set.append(find_similarity(reference_image, additional_image_data))
 
+				reverse_zoom_effect(additional_image_path, original_image_data)
+
 		similarity_threshold = sum(solutions_set) / len(solutions_set) * 3
+
+		reverse_zoom_effect(reference_path, original_reference_data)
 
 		with open("similarity_threshold.txt", "w") as file:
 			file.write(str(similarity_threshold))
 
-
 	try:
 		prGreen(">> Working...")
+
+		original_image_data = copy_image_data(comparison_path)
+		original_reference_data = copy_image_data(reference_path)
+
+		zoom_effect(reference_path, zoom_factor)
+		zoom_effect(comparison_path, zoom_factor)
 
 		comparison_image = fetch_image_data(comparison_path)
 		similarity_percentile += find_similarity(reference_image, comparison_image)
 
-		comparison_image = fetch_blackwhite_data(comparison_path)
+		comparison_image = fetch_blackwhite_data(copy_image_path)
 		similarity_percentile += find_similarity(reference_image, comparison_image)
 
-		comparison_image = fetch_greyscale_data(comparison_path)
+		comparison_image = fetch_greyscale_data(copy_image_path)
 		similarity_percentile += find_similarity(reference_image, comparison_image)
+
+		reverse_zoom_effect(comparison_path, original_image_data)
+		reverse_zoom_effect(reference_path, original_reference_data)
 	except:
 		prRed(">> Error: Image Not Found.")
 		exit()
